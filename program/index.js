03d0bd9950a4690e2051f68f3e417c50e5d4c9a9
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-16 18:37:40 +0100

    added basic server function

--- program/index.js
+++ program/index.js
@@ -1,1 +1,10 @@
 require("dotenv").config();
+const http = require("http");
+
+const port = process.env.PORT || 4000;
+const server = http.createServer();
+server.listen(port);
+
+server.on("request", (request, response) => {
+  response.end();
+});
