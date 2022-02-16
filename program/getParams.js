371953701c2a99e860fbd952de21f142e1c8f81d
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-16 18:53:18 +0100

    added param invalidaty detection

--- program/getParams.js
+++ program/getParams.js
@@ -3,12 +3,19 @@ const getParams = (url) => {
   if (!url.includes("?")) {
     return "No params detected";
   }
+
   const paramsString = url.split("?")[1];
+
+  if (paramsString === "") {
+    return "Invalid params";
+  }
   const params = paramsString.split("&");
 
   params.forEach((param) => {
     const [paramName, paramValue] = param.split("=");
-    paramsObject[paramName] = paramValue;
+    if (paramValue !== undefined) {
+      paramsObject[paramName] = paramValue;
+    }
   });
 
   return paramsObject;
