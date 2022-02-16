3252e60bafccefe71b5ff32d0adacd9fbd484bfb
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-16 20:27:20 +0100

    mounted a file based server

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 require("dotenv").config();
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const getParams = require("./program/getParams");
 
 const port = process.env.PORT || 4000;
 const server = http.createServer();
-server.listen(port);
+
+const dir = path.join(__dirname, "public");
+const mime = {
+  html: "text/html",
+  txt: "text/plain",
+  css: "text/css",
+  gif: "image/gif",
+  jpg: "image/jpeg",
+  png: "image/png",
+  svg: "image/svg+xml",
+  js: "application/javascript",
+};
 
 server.on("request", (request, response) => {
   const params = getParams(request.url);
-  console.log(params);
-  response.end();
+
+  const requestPath = request.url.toString().split("?")[0];
+
+  if (request.method !== "GET") {
+    response.statusCode = 501;
+    response.setHeader("Content-Type", "text/plain");
+    response.write("<h1>Method not implemented<h1>");
+    response.end();
+  }
+
+  let file = path.join(dir, requestPath.replace(/\/$/, "/index.html"));
+
+  if (file.split(".").length !== 2) {
+    file += ".html";
+  }
+
+  if (!request.url.includes("calculator")) {
+    if (file.indexOf(dir + path.sep) !== 0) {
+      response.statusCode = 403;
+      response.setHeader("Content-Type", "text/plain");
+      response.write("<h1>Forbidden<h1>");
+      response.end();
+    }
+
+    const type = mime[path.extname(file).slice(1)] || "text/plain";
+    const dataStream = fs.createReadStream(file);
+
+    dataStream.on("open", () => {
+      response.setHeader("Content-Type", type);
+      dataStream.pipe(response);
+    });
+
+    dataStream.on("error", () => {
+      response.setHeader("Content-Type", "text/plain");
+      response.statusCode = 404;
+      response.end("Not found");
+    });
+  }
 });
+
+server.listen(port);
